d7353209e29d82dd46e9b0dcd9f8d66579721004
muditsingal <muditsingal@gmail.com>
2023-06-14 22:57:08 +0530

    changes in tflite converter python code

--- hardware/TF_lite/convert_model_to_tflite.py
+++ hardware/TF_lite/convert_model_to_tflite.py
@@ -1,12 +1,34 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+import os
+
+curr_pwd = os.getcwd()
 
 # Set the path to the directory containing the TensorFlow model
-saved_model_dir = '/model'
+saved_model_dir = os.path.join('model', 'target_actor_td3')
 
-# Convert the TensorFlow model to TensorFlow Lite format
-converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=20),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(2, activation='tanh')
+])
+
+# model = tf.keras.Sequential([
+#     tf.keras.layers.InputLayer(input_shape=20),
+#     tf.keras.layers.Dense(400, activation='relu'),
+#     tf.keras.layers.Dense(300, activation='relu'),
+#     tf.keras.layers.Dense(2, activation='tanh')
+# ])
+
+model.compile(optimizer=Adam(learning_rate=0.0005), loss='mean_squared_error')
+model.load_weights(saved_model_dir)
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_model = converter.convert()
+tflite_model_name = 'lite_model'
 
-# Save the TensorFlow Lite model to a file
-with open('model.tflite', 'wb') as f:
-    f.write(tflite_model)
+open(tflite_model_name + '.tflite', 'wb').write(tflite_model)
